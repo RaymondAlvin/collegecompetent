04562cae3adee8c073921edc4c02b40af48bb39a
Raymond Alvin <raymondalvin@Raymonds-MacBook-Pro.local>
2023-06-30 10:41:38 -0700

    changed navbar and added flag

--- src/Navbar.js
+++ src/Navbar.js
@@ -8,7 +8,9 @@ function Navbar() {
       <header class="d-flex flex-wrap justify-content-center py-3 border-bottom" >
         <Link to="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
           <svg class="bi me-2" width="40" height="32"><use xlinkHref="#bootstrap"/></svg>
-          <span class="fs-4">College Competent</span>
+          <img src={require("/Users/raymondalvin/Desktop/react/collegecompetent/src/images/pennant-e1688146797850.png")} width="120" height="45" class="d-inline-block align-top" alt=""/>
+          <span class="fs-4"></span>
+          
         </Link>
   
         <ul class="nav nav-pills">
@@ -18,6 +20,30 @@ function Navbar() {
         </ul>
       </header>
     </div>
+//     <div>
+//         <nav className="navbar navbar-light bg-light navbar navbar-expand-lg navbar-light bg-light">
+//             <Link className="navbar-brand" to="home">
+//                 <img src={require("/Users/raymondalvin/Desktop/react/collegecompetent/src/images/pennant.png")} width="45" height="45" class="d-inline-block align-top" alt=""/>
+//                 ION Config Database
+//             </Link>
+//             <div  id="navbarNav">
+//             <ul className="navbar-nav">
+//                 <li className="nav-item active">
+//                     <Link className="nav-link" to="view">View </Link>
+//                 </li>
+//                 <li className="nav-item">
+//                     <Link className="nav-link" to="query">Query</Link>
+//                 </li>
+//                 <li className="nav-item">
+//                     <Link className="nav-link" to="input">Input</Link>
+//                 </li>
+//                 <li className="nav-item">
+//                     <Link className="nav-link" to="login">Login</Link>
+//                 </li>
+//             </ul>
+//   </div>
+//         </nav>
+//     </div>
   )
 }
 
