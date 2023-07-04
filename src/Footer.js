c002f9b1ff4849cde8ec5a4c137a9943d556907f
Raymond Alvin <raymondalvin@Raymonds-MacBook-Pro.local>
2023-07-03 22:51:45 -0700

    updated navbar and footer

--- src/Footer.js
+++ src/Footer.js
@@ -7,8 +7,9 @@ function Footer() {
     <footer className="py-3 my-4">
       <ul className="nav justify-content-center border-bottom pb-3 mb-3">
         <li className="nav-item"><Link to="/" className="nav-link px-2 text-body-secondary">Home</Link></li>
-        <li className="nav-item"><Link to="/collegeconsulting" className="nav-link px-2 text-body-secondary">College Consulting</Link></li>
-        <li className="nav-item"><Link to="/essayhelp" className="nav-link px-2 text-body-secondary">Essay Help</Link></li>
+        <li className="nav-item"><Link to="/essayreading" className="nav-link px-2 text-body-secondary">Essay Reading</Link></li>
+        <li className="nav-item"><Link to="/thecompletepackage" className="nav-link px-2 text-body-secondary">The Complete Package</Link></li>
+        <li className="nav-item"><Link to="/activitiesenhancer" className="nav-link px-2 text-body-secondary">Activities Enhancer</Link></li>
         <li className="nav-item"><Link to="/about" className="nav-link px-2 text-body-secondary">About</Link></li>
       </ul>
       <p className="text-center text-body-secondary">&copy; 2023 College Competent</p>
--- src/Navbar.js
+++ src/Navbar.js
@@ -4,46 +4,43 @@ import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
+    
     <div class="container">
       <header class="d-flex flex-wrap justify-content-center py-3 border-bottom" >
         <Link to="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
           <svg class="bi me-2" width="40" height="32"><use xlinkHref="#bootstrap"/></svg>
           <img src={require("/Users/raymondalvin/Desktop/react/collegecompetent/src/images/pennant-e1688146797850.png")} width="120" height="45" class="d-inline-block align-top" alt=""/>
           <span class="fs-4"></span>
-          
         </Link>
   
         <ul class="nav nav-pills">
-          <li class="nav-item"><Link to="/collegeconsulting" class="nav-link">College Consulting</Link></li>
-          <li class="nav-item"><Link to="/essayhelp" class="nav-link">Essay Help</Link></li>
+          <li class="nav-item"><Link to="/essayreading" class="nav-link">Essay Reading</Link></li>
+          <li class="nav-item"><Link to="/thecompletepackage" class="nav-link">The Complete Package</Link></li>
+          <li class="nav-item"><Link to="/activitiesenhancer" class="nav-link">Activities Enhancer</Link></li>
           <li class="nav-item"><Link to="/about" class="nav-link">About</Link></li>
         </ul>
       </header>
     </div>
-//     <div>
-//         <nav className="navbar navbar-light bg-light navbar navbar-expand-lg navbar-light bg-light">
-//             <Link className="navbar-brand" to="home">
-//                 <img src={require("/Users/raymondalvin/Desktop/react/collegecompetent/src/images/pennant.png")} width="45" height="45" class="d-inline-block align-top" alt=""/>
-//                 ION Config Database
-//             </Link>
-//             <div  id="navbarNav">
-//             <ul className="navbar-nav">
-//                 <li className="nav-item active">
-//                     <Link className="nav-link" to="view">View </Link>
-//                 </li>
-//                 <li className="nav-item">
-//                     <Link className="nav-link" to="query">Query</Link>
-//                 </li>
-//                 <li className="nav-item">
-//                     <Link className="nav-link" to="input">Input</Link>
-//                 </li>
-//                 <li className="nav-item">
-//                     <Link className="nav-link" to="login">Login</Link>
-//                 </li>
-//             </ul>
-//   </div>
-//         </nav>
-//     </div>
+    
+    // <div class="container fixed-top">
+    //   <header class="d-flex flex-wrap justify-content-center py-3 navbar" >
+    //     <Link to="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none ">
+    //       <svg class="bi me-2" width="40" height="0"><use xlinkHref="#bootstrap"/></svg>
+    //       <img src={require("/Users/raymondalvin/Desktop/react/collegecompetent/src/images/pennant-e1688146797850.png")} width="120" height="45" class="d-inline-block align-top" alt=""/>
+    //       <span class="fs-4 "></span>
+    //     </Link>
+  
+    //     <ul class="nav nav-pills">
+    //       <li class="nav-item"><Link to="/collegeconsulting" class="nav-link">College Consulting</Link></li>
+    //       <li class="nav-item"><Link to="/essayhelp" class="nav-link">Essay Help</Link></li>
+    //       <li class="nav-item"><Link to="/about" class="nav-link">About</Link></li>
+    //     </ul>
+    //   </header>
+    // </div>
+            
+  
+
+
   )
 }
 
